60a6978ab953bc961d5bb08596a6b0c1712c08b2
ankitgupta167 <ankitpgupta051@gmail.com>
2025-09-02 15:23:50 +0530

    changes-7

--- backend/app.js
+++ backend/app.js
@@ -37,7 +37,7 @@ app.use("/api/v1/application", applicationRouter);
 
 app.get("/",(req, res,next)=>{return res.status(200).json({
     success: true,
-    message : "hello world ka hal cal ba"
+    message : "hello world ka hal cal ba  kaye project nhi chal rha h"
 
 })})
  dbConnection();
