af1204849aa623947f9417b02d5feb408b66c3df
ankitgupta167 <ankitpgupta051@gmail.com>
2025-09-05 00:13:43 +0530

    url-changes

--- frontend/src/components/Job/JobDetails.jsx
+++ frontend/src/components/Job/JobDetails.jsx
@@ -12,7 +12,9 @@ const JobDetails = () => {
 
   useEffect(() => {
     axios
-      .get(`http://localhost:4000/api/v1/job/${id}`, {
+      // .get(`http://localhost:4000/api/v1/job/${id}`,
+        
+      .get(`https://job-plateform.onrender.com/api/v1/job/${id}`,  {
         withCredentials: true,
       })
       .then((res) => {
--- frontend/src/components/Job/Jobs.jsx
+++ frontend/src/components/Job/Jobs.jsx
@@ -10,8 +10,10 @@ const Jobs = () => {
   useEffect(() => {
     try {
       axios
-        .get("http://localhost:4000/api/v1/job/getall", {
-          withCredentials: true,
+        // .get("http://localhost:4000/api/v1/job/getall", {
+        
+        .get("https://job-plateform.onrender.com/api/v1/job/getall",{
+        withCredentials: true,
         })
         .then((res) => {
           setJobs(res.data);
